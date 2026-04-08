18b2060fb5edda0867feaee4fbdb0e0cb2fedafb
yevheniihudzinskyi <gudzinskyi@gmail.com>
2026-04-08 18:16:52 +0300

    policy

---
+++ app/Http/Requests/StoreAuthorRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAuthorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // Set to true if the user is authorized to perform the store action.
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'country' => 'required|string',
+        ];
+    }
+}
